3bed47929fadc969ff0783a5f47402c70274ec63
Dominika Limanówka <47063149+xlimiii@users.noreply.github.com>
2020-05-08 10:37:02 +0200

    Piłkarze MVVM

---
+++ MVVM_Pilkarze/MVVM_Pilkarze/Model/Druzyna.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace MVVM_Pilkarze.Model
+{
+    class Druzyna
+    {
+        public List<Pilkarz> Sklad { get; set; }
+        public Druzyna()
+        {
+            Sklad = new List<Pilkarz>();
+            WczytajDruzyne(@"Pilkarze.json");
+        }
+
+        public void WczytajDruzyne(string plik)
+        {
+            if (File.Exists(plik))
+            {
+                string tekst = File.ReadAllText(plik);
+                Sklad = JsonConvert.DeserializeObject<List<Pilkarz>>(tekst);   
+            }
+        }
+
+        public void ZapiszDruzyne(string plik)
+        {
+            string jsonFormat = JsonConvert.SerializeObject(Sklad);
+            File.WriteAllText(plik, jsonFormat);
+
+        }
+
+        public void  DodajPilkarza(Pilkarz p)
+        {
+            Sklad.Add(p);
+        }
+
+        public void UsunPilkarza(int i)
+        {
+            Sklad.RemoveAt(i);
+        }
+
+        public void EdytujPilkarza(Pilkarz p, int i)
+        {
+            Sklad[i] = p;
+        }
+        public List<Pilkarz> ZwrocPilkarza
+        {
+            get => Sklad;
+        }
+
+
+    }
+}
---
+++ MVVM_Pilkarze/MVVM_Pilkarze/Model/Pilkarz.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_Pilkarze.Model
+{
+    internal class Pilkarz
+    {
+
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public uint Wiek { get; set; }
+        public uint Waga { get; set; }
+
+        public Pilkarz(string imie, string nazwisko, uint wiek, uint waga)
+        {
+            Imie = imie;
+            Nazwisko = nazwisko;
+            Wiek = wiek;
+            Waga = waga;
+        }
+
+        public Pilkarz(string imie, string nazwisko) : this(imie, nazwisko, 30, 75) { }
+
+        public Pilkarz(Pilkarz p)
+        {
+            Imie = p.Imie;
+            Nazwisko = p.Nazwisko;
+            Wiek = p.Wiek;
+            Waga = p.Waga;
+        }
+        public Pilkarz()
+        {
+
+        }
+        public override string ToString()
+        {
+            return $"{Nazwisko} {Imie} lat: {Wiek} waga: {Waga} kg";
+        }
+        public bool CzyTakiSam(Pilkarz p)
+        {
+            if (p.Nazwisko != Nazwisko)
+                return false;
+            if (p.Imie != Imie)
+                return false;
+            if (p.Wiek != Wiek)
+                return false;
+            if (p.Waga != Waga)
+                return false;
+            return true;
+        }
+
+    }
+}
